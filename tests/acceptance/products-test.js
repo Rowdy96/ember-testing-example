6b813b71dbca7ddd3cfdf57d75cfe9e886240801
Rowdy96 <rahul.arambagh96@gmail.com>
2024-06-06 17:13:41 +0530

    move data generators

--- tests/acceptance/products-test.js
+++ tests/acceptance/products-test.js
@@ -2,32 +2,13 @@ import { module, test } from 'qunit';
 import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-testing-example/tests/helpers';
 import Pretender from 'pretender';
+import { getProductList } from './../data-generators';
 
 module('Acceptance | products', function (hooks) {
   setupApplicationTest(hooks);
 
   test('visiting /products', async function (assert) {
     const server = new Pretender();
-    const getProductList = () => [
-      {
-        title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-        category: "men's clothing",
-        description: 'Your perfect pack for everyday use',
-        id: 1,
-        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-        price: 109.95,
-        rating: { rate: 3.9, count: 120 },
-      },
-      {
-        title: 'Fjallraven 2',
-        category: "men's clothing",
-        description: 'Your perfect pack for everyday use',
-        id: 1,
-        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-        price: 109.95,
-        rating: { rate: 3.9, count: 120 },
-      },
-    ];
 
     server.get('https://fakestoreapi.com/products', () => [
       200,
---
+++ tests/data-generators.js
@@ -0,0 +1,20 @@
+export const getProductList = () => [
+    {
+        title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+        category: "men's clothing",
+        description: 'Your perfect pack for everyday use',
+        id: 1,
+        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+        price: 109.95,
+        rating: { rate: 3.9, count: 120 },
+    },
+    {
+        title: 'Fjallraven 2',
+        category: "men's clothing",
+        description: 'Your perfect pack for everyday use',
+        id: 1,
+        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+        price: 109.95,
+        rating: { rate: 3.9, count: 120 },
+    },
+];
